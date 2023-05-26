08861164e372e0076e542fda8dcb6b2488d76353
MaryahKim <maryapitaz@gmail.com>
2023-05-26 15:24:48 +0300

    Add fonts with extendedTheme prop

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import App from './App.tsx';
-import './index.css';
+
+const fonts = {
+	body: 'Playfair Display',
+	heading: 'Libre Bakserville',
+};
+const theme = extendTheme({ fonts });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
 	</React.StrictMode>
