4ca53a994c489ed85e9fb70a616b0a3c986ee2f6
MaryahKim <maryapitaz@gmail.com>
2023-06-01 14:00:46 +0300

    Create search Jobs Component

---
+++ src/components/SearchJobs.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '@chakra-ui/react';
+
+export default function SearchJobs() {
+	return (
+		<>
+			<Container>
+				<Text>Main Content Goes Here!</Text>
+			</Container>
+		</>
+	);
+}
