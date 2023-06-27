95e4803f7c42b3792c0d06d3b85f52df06ebeb2e
MaryahKim <maryapitaz@gmail.com>
2023-06-27 14:18:50 +0300

    import jobfilter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar';
 import AppDescription from './components/AppDescription';
 import SearchJobs from './components/SearchJobs';
+import JobFilter from './components/JobFilter';
 
 const App = () => {
 	return (
@@ -8,6 +9,7 @@ const App = () => {
 			<NavBar />
 			<AppDescription />
 			<SearchJobs />
+			<JobFilter />
 		</>
 	);
 };
